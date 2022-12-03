d152b76ba240524fda5192721584b8dda4ec387e
iosifx <sdi1800299@di.uoa.gr>
2022-12-03 19:09:18 +0200

    avatar changes

--- Avatar.cpp
+++ Avatar.cpp
@@ -4,5 +4,10 @@
 using namespace std;
 
 class Avatar : public God {
-    
+    private:
+        int MagicPot = 1;      // Magic pot initialize to 1 at the start of the game
+        char Team_Selection ;
+        
+    public :
+
 }
