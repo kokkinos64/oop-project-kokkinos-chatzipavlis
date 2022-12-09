27f1d99ed155b3da6ea6658332642b1c03bf50ce
sdi2000084 <sdi2000084@di.uoa.gr>
2022-12-09 14:06:02 +0200

    Added input to the client

--- client.cpp
+++ client.cpp
@@ -4,9 +4,36 @@
 #include "God.h"
 #include "Werewolf.h"
 #include "Vampire.h"
+#include "Matrix.h"
+
+using namespace std;
 
 int main(void)
 {
+    // VARIABLES
+    int x, y;
+    char team;
+
+    // INPUT: MAP DIMENTIONS
+    cout << "Enter the map dimentions: ";
+    cin >> x >> y;
+    Matrix Map(x,y);    // Create the matrix
+    
+
+    // INPUT: TEAM SELECTION
+    cout << "Select a side (w,v): ";
+    cin >> team;
+
+    while (team != 'w' || team != 'v')
+    {
+        cout << "Invalid input. Usage: w for Werewolves, v for Vampires.\n";
+        cin >> team;
+    }
+
+
+
+
+
 
     return 0;
 }
