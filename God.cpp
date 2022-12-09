9210063584c6d8390db2548997956d2076b9f65a
sdi2000084 <sdi2000084@di.uoa.gr>
2022-12-09 09:56:27 +0200

    Proper split of the God class into Interface/Implementation files

--- God.cpp
+++ God.cpp
@@ -1,30 +1,25 @@
 #include <iostream>
+#include "God.h"
 
 using namespace std;
 
-class God {
-    private:
-        int Health;
-        int Meds;
-        const int Attack;
-        const int Defense;
+int God::getHealth(void)
+{
+    return Health;
+}
 
-    public:
-        int getHealth(void)
-        {
-            return Health;
-        }
+int God::getAttack(void)
+{
+    return Attack;
+}
 
-        int getAttack(void)
-        {
-            return Attack;
-        }
+int God::getDefense(void)
+{
+    return Defense;
+}
 
-        int getDefense(void)
-        {
-            return Defense;
-        }
-};
+
+
 
 // Global counters for the members  of each team, initialized to 0.
 int WerewolfCount = 0, VampireCount = 0;
--- God.h
+++ God.h
@@ -1,5 +1,11 @@
 class God
 {
+    private:
+        int Health;
+        int Meds;
+        const int Attack;
+        const int Defense;
+
     public:
         int getHealth(void);
         int getAttack(void);
