25f205391fc80ce45b64ad76ef0978c76f0d2067
sdi2000084 <sdi2000084@di.uoa.gr>
2022-12-17 13:50:28 +0200

    Writing the new client (in progress)

--- client.cpp
+++ client.cpp
@@ -1,66 +1,32 @@
-// This is the client code to test the game
-
 #include <iostream>
-#include <cstdlib>
-
 #include "Characters.h"
-#include "Map.h"
-#include "Matrix.h"
+#include "Map2.h"
 
 using namespace std;
 
 int main(void)
 {
-    srand((unsigned)time(NULL));    // Used by rand later
+	int x, y;
+	char team;
 
-    // VARIABLES
-    int x, y;
-    char team;
+	// Create the map
+	cout << "Enter map dimentions (x y): ";
+	cin >> x >> y;
+	Map M(x, y);
 
-    // INPUT: Board DIMENTIONS
-    cout << "Enter the Board dimentions: ";
-    cin >> x >> y;
-    Matrix Board(x,y);    // Create the matrix
-    
-    // INPUT: TEAM SELECTION
-    cout << "Select a side (w,v): ";
-    cin >> team;
+	// Team selection
+	cout << "Select your team (w, v): ";
+	cin >> team;
 
-    // Check for invalid input
-    while ((team != 'w') && (team != 'v'))
-    {
-        cout << "Invalid input. Usage: w for Werewolves, v for Vampires.\n";
-        cin >> team;
-    }
-    
-    // In case of valid input,
-    cout << "Your team: ";
+	while ((team != 'w') && (team != 'v'))	// Check for invalid input
+	{
+		cout << "Invalid input. Usage: w for Werewolves, v for Vampires.\n";
+		cin >> team;
+	}
 
-    switch (team)
-    {
-    case 'w':
-        cout << "Werewolves";
-        break;
-    case 'v':
-        cout << "Vampires";
-        break;
-    }
-
-    cout << endl;
+	Avatar A(team);
 
 
-    // Objects
-    Avatar a(team); // Create the avatar, pass thru the team selection
-    
-    Board.MatrixGroundFill();
 
-    Board.MatrixPrint();
-
-    Board.MatrixInsertAt(5, 5, 'x');
-
-    Board.MatrixPrint();
-
-    Board.MatrixDelete();  // Delete the matrix
-    return 0;
+	return 0;
 }
-
