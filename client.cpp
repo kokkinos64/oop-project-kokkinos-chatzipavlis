c638ea0ba41c187621504202f3047b661a2ba1e8
IosifChatz <iosifxatzipaulis@gmail.com>
2022-12-22 16:16:40 +0200

    Create combat conditions and local variables for combat conditions

--- client.cpp
+++ client.cpp
@@ -54,35 +54,53 @@ int main(void)
 	
 	A.Move();
 
-		while (A.VampireCount != 0 || A.WerewolfCount != 0)
-		{
-			M.PrintMap();
+	while (A.VampireCount != 0 || A.WerewolfCount != 0)
+	{
+		M.PrintMap();
 
-			// Change the time of day
-			if ((A.DaytimeCounter %= 5) == 0) {
-				A.isDaytime = !(A.isDaytime);	// Switch daytime
-				cout << "Daytime has changed\n";
+		// Change the time of day
+		if ((A.DaytimeCounter %= 5) == 0) {
+			A.isDaytime = !(A.isDaytime);	// Switch daytime
+			cout << "Daytime has changed\n";
+		}
+
+		// Monster movements
+		for (int i = 0; i < MonstersNum; i++)
+		{
+			// Check health status
+			if (WerewolfVector.at(i)->getHealth() == 0)
+			{
+				delete WerewolfVector.at(i);
+				A.WerewolfCount--;
 			}
 
-			// Monster movements
-			for (int i = 0; i < MonstersNum; i++)
+			if (VampireVector.at(i)->getHealth() == 0)
 			{
-				// Check health status
-				if (WerewolfVector.at(i)->getHealth() == 0)
-				{
-					delete WerewolfVector.at(i);
-					A.WerewolfCount--;
-				}
+				delete VampireVector.at(i);
+				A.VampireCount--;
+			}
 
-				if (VampireVector.at(i)->getHealth() == 0)
-				{
-					delete VampireVector.at(i);
-					A.VampireCount--;
-				}
+			// Move monsters
+			WerewolfVector.at(i)->WerewolfMove();
+			VampireVector.at(i)->VampireMove();
 
-				// Move monsters
-				WerewolfVector.at(i)->WerewolfMove();
-				VampireVector.at(i)->VampireMove();
+			// Combat condition check
+
+			int WerewolfX = WerewolfVector.at(i)->getX();
+			int WerewolfY = WerewolfVector.at(i)->getY();
+			int VampireX = VampireVector.at(i)->getX();
+			int VampireY = VampireVector.at(i)->getY();
+
+
+			if (((WerewolfX == VampireX - 1)) || (WerewolfX == VampireX + 1) || ((WerewolfX == VampireX -1) && (WerewolfX == VampireY - 1)) || ((WerewolfX == VampireX - 1) && (WerewolfX == VampireY + 1)) || ((WerewolfX == VampireX + 1) && (WerewolfX == VampireY - 1)) || ((WerewolfX == VampireX + 1) && (WerewolfX == VampireY + 1))) {
+				if (((WerewolfY == VampireY - 1)) || (WerewolfY == VampireY + 1)) {
+					if (WerewolfVector.at(i)->getAttack() > VampireVector.at(i)->getAttack()) {
+						VampireVector.at(i)->HealthDecreaseBy(WerewolfVector.at(i)->getAttack() - VampireVector.at(i)->getDefense());
+					}
+					else if ((VampireVector.at(i)->getAttack() > WerewolfVector.at(i)->getAttack())) {
+						WerewolfVector.at(i)->HealthDecreaseBy(VampireVector.at(i)->getAttack() - WerewolfVector.at(i)->getDefense());
+					}
+				}
 			}
 
 			M.PrintMap();
@@ -93,7 +111,7 @@ int main(void)
 
 			A.Move();
 		}
-	
+	}
 
 		cout << "A team has been eliminated.\n\nGAME OVER\n";
 		system("timeout 5");
